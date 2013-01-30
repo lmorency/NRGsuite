e4867eb63d30773ac87c79f486757150a1a519fe
Francis Gaudreault <francis@Chopin.local>
2013-01-30 01:39:00 -0500

    TargetFlex file restored

---
+++ FlexAID/TargetFlex.py
@@ -0,0 +1,35 @@
+class TargetFlex:
+
+    def __init__(self):
+
+        self.listSideChain = list()
+
+    ''' ==================================================================================
+    FUNCTION Add_SideChain: Adds a flexible side chain to array list
+    ==================================================================================  '''           
+    def Add_SideChain(self, Residue):
+
+        if self.listSideChain.count(Residue) == 0:
+            self.listSideChain.append(Residue)
+
+    ''' ==================================================================================
+    FUNCTION Remove_SideChain: Removes a flexible side chain to array list
+    ==================================================================================  '''           
+    def Remove_SideChain(self, Residue):
+
+        if self.listSideChain.count(Residue) != 0:
+            self.listSideChain.remove(Residue)
+
+    ''' ==================================================================================
+    FUNCTION Clear_SideChain: Empties the list of flexible side chain
+    ==================================================================================  '''           
+    def Clear_SideChain(self):
+
+        del self.listSideChain[:]
+
+    ''' ==================================================================================
+    FUNCTION Count_SideChain: Returns the number of flexible side chain
+    ==================================================================================  '''           
+    def Count_SideChain(self):
+
+        return len(self.listSideChain)
