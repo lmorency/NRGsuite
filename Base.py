bf463b4b4c3f5222c0ab038013d815bbe540219c
francis <francisgaudreault@dsNksT.local>
2014-11-06 12:44:29 -0500

    Changed the color of the flexible side-chain selections to match ligand flexible bonds. Changed the version number in Base.py.

--- Base.py
+++ Base.py
@@ -38,7 +38,7 @@ class Base:
     TKINTER_UPDATE_INTERVAL = 100
     
     WEBSITE = 'http://bcb.med.usherbrooke.ca/'
-    VERSION = '2.45a'
+    VERSION = '2.45b'
     
     ''' ==================================================================================
     FUNCTION __init__ : Initialization of the variables of the interface
